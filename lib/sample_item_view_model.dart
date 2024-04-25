bc00a84f8f2b177cf12c42ad59768206be272642
tungharu03 <96756227+tungharu03@users.noreply.github.com>
2024-04-25 13:43:21 +0700

    Update sample_item_view_model.dart

--- lib/sample_item_view_model.dart
+++ lib/sample_item_view_model.dart
@@ -1,11 +1,16 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'sample_item.dart';
 
 class SampleItemViewModel extends ChangeNotifier {
+  static final _instance = SampleItemViewModel._();
+  factory SampleItemViewModel() => _instance;
+  SampleItemViewModel._();
+
   final List<SampleItem> items = [];
 
   void addItem(String name) {
-    items.add(SampleItem(id: SampleItem.generateUuid(), name: name));
+    items.add(SampleItem(name: name));
     notifyListeners();
   }
 
@@ -17,8 +22,7 @@ class SampleItemViewModel extends ChangeNotifier {
   void updateItem(String id, String newName) {
     try {
       final item = items.firstWhere((item) => item.id == id);
-      item.name = newName;
-      notifyListeners();
+      item.name.value = newName;
     } catch (e) {
       debugPrint("Không tìm thấy mục với ID $id");
     }
